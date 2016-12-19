742ccacd4de7d6896d5e7b62172431ba61284d76
Ralph Noel O. Bonifacio <pargfxstudio@gmail.com>
2016-12-19 20:29:45 +0800

    deleted

--- exercise7/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=guestform',
-    'username' => 'root',
-    'password' => 'parpar21',
-    'charset' => 'utf8',
-];
