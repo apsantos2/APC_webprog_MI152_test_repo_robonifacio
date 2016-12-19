f498543c2cecc7cfeeffbc79ba4548cf0423cda3
Ralph Noel O. Bonifacio <pargfxstudio@gmail.com>
2016-12-19 20:32:39 +0800

    exercise7 with trivias and guestform

---
+++ exercise7/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=guestform',
+    'username' => 'root',
+    'password' => 'parpar21',
+    'charset' => 'utf8',
+];
