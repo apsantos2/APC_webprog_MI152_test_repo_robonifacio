6c08f8c59fde917f6c981f14be0dbbc3f34b7c6b
Ralph Noel O. Bonifacio <pargfxstudio@gmail.com>
2016-12-22 00:46:15 +0800

    added go back and design for trivia view

--- exercise7/views/trivia/view.php
+++ exercise7/views/trivia/view.php
@@ -10,10 +10,16 @@ $this->title = $model->ID;
 $this->params['breadcrumbs'][] = ['label' => 'Trivias', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<div id="large-header" class="large-header">
+    <canvas id="demo1-canvas"></canvas>
+			<div class="points">
+				<canvas id="demo-canvas"></canvas>
+			</div>
+			<div class="triviav-title">		
 <div class="trivia-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
-
+	<center><p><a href="http://127.0.0.1/exercise7/web/index.php?r=trivia" class="btn btn-lg btn-success">GO BACK</a></p></center>
     <p>
         <?= Html::a('Update', ['update', 'id' => $model->ID], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'id' => $model->ID], [
@@ -34,4 +40,7 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]) ?>
 
+</div>
+</div>
+</div>
 </div>
